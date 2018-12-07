b0bc423e1c24e1fed01e46a3827743f64f8acf65
Marius Mandal <mariusmandal@gmail.com>
2018-12-07 15:36:05 +0100

    bruk riktig ID

--- rest_api/nettside.php
+++ rest_api/nettside.php
@@ -10,15 +10,13 @@ function UKMnettside_api() {
 	
 	$items = [];
 	foreach( $menu_items as $menu_item ) {
-
-        
 		$item = new stdClass();
-		$item->id   		= $menu_item->ID;
+		$item->id   		= $menu_item->object_id;
 		$item->title     	= $menu_item->title;
 		$item->lead 	    = 'Ingress kommer her etter hvert';
         $item->url  		= $menu_item->url;
         $item->image        = 'https://placehold.it/300x169/';
-        $item->contenturl   = 'https://ukm.no/testfylke/wp-json/UKM/content/'. $menu_item->ID;
+        $item->contenturl   = 'https://ukm.no/testfylke/wp-json/UKM/content/'. $menu_item->object_id;
         
         $items[] = $item;
 	}
